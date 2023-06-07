c3e8c2c809fa755cfa9010ebd245c97590b4c7fa
assomdevgit <assomggm@gmail.com>
2023-06-07 15:48:18 +0900

    Initial commit

---
+++ 02_세미콜론.py
@@ -0,0 +1,9 @@
+# 기본 문법
+# 세미콜론(;)
+# - 많은 프로그래밍 언어들 --> 하나의 구문이 끝날때 마다 세미콜론(;)을 붙여야함.
+# 파이션은 같은 줄에 여러 구문을 넣을 때만 세미콜론(;)을 넣는다.
+print("Hello Python"); # 세미콜론을 붙여도 에러가 나진 않음.
+print("Hello Python")
+print("Hello Python")
+print("Hello Python")
+print("Hello Python1"); print("Hello Python2")
