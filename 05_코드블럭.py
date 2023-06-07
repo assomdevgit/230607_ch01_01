f2865c2fdd95a6a385f9b87f975a7207051d91f7
assomdevgit <assomggm@gmail.com>
2023-06-07 16:14:05 +0900

    [업데이트] 깃허브 계정 변경하기

--- 05_코드블럭.py
+++ 05_코드블럭.py
@@ -8,3 +8,10 @@ if a == 10:
     # print("못 믿으시겠어요?")
     #     print("a가 10이 이에") <-- 오류 코드
 print("못 믿으시겠어요?")
+print("vcs >> GitHub 프로젝트 공유 클릭")
+# GitHub 프로젝트 공유를 하였더니, 상단 도구모음이 Git으로 변경되었다.
+# 커밋 및 여러 기능들이 생성되어서 컨트롤할 수 있다.
+# git Bash에서 git config --global user.email "XXXXXXXXXXXXXXX"
+# git Bash에 git config --global user.name "XXXXXXXXXXXXXXX"
+# gitHub 기존 등록된 계정 삭제하는 방법은 ...
+# windows 자격증명 (제어판>사용자 계정>자격증명관리>> windows자격증명 >> 일반 자격 증명에서 github 제거
